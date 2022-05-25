c97f143287d93664c2ebc862e4ab39d65fe99ede
henry <phamtiensy8696@gmail.com>
2022-05-25 13:07:57 +0700

    add web mvc

---
+++ Booking_Hotel.Web/Controllers/ArticleController.cs
@@ -0,0 +1,29 @@
+﻿using Booking_Hotel.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booking_Hotel.Web.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ILogger<ArticleController> _logger;
+
+        public ArticleController(ILogger<ArticleController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("/tin-tuc/{page?}", Name = "article-default")]
+        [Route("/tin-tuc/{page?}/{catid?}/{title?}", Name = "article")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     
+    }
+}
---
+++ Booking_Hotel.Web/Controllers/ReservationController.cs
@@ -0,0 +1,28 @@
+﻿using Booking_Hotel.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booking_Hotel.Web.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly ILogger<ReservationController> _logger;
+
+        public ReservationController(ILogger<ReservationController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("/dat-phong", Name = "reservation")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
